ed49fe2776765b8dc4700ae62337fff222db9fb8
adityathebe <thebeaditya@gmail.com>
2017-07-02 22:28:34 +0545

    Async !!

--- index.js
+++ index.js
@@ -2,50 +2,43 @@ let request = require('request');
 let cheerio = require('cheerio');
 let express = require('express');
 let cors = require('cors');
+
 let app = express();
 app.use(cors());
 
-/* === Store Data Here === */
-let retail;
-let wholesale;
+let urlR = "http://kalimatimarket.gov.np/home/rpricelist";
+let urlW = "http://kalimatimarket.gov.np/home/wpricelist";
 
-function getData(res, option) {
-	let url;
-	if(option == 1)
-		url = "http://kalimatimarket.gov.np/home/rpricelist";
-	else
-		url = "http://kalimatimarket.gov.np/home/wpricelist";
-
-	request(url, (error, response, body)=> {
-	    let data = [];
-
-	    /* ------ Cheerio -------- */
+app.get('/', (req, res) => {
+	request(urlR, (error, response, body)=> {
+		let retail = [];
 	    let $ = cheerio.load(body);
 	    $('tr').each(function() {
 	    	let temp = [];
 	    	$(this).find('td').each(function(i) {
 	      		temp.push($(this).text());
 	    	});
-	    	data.push(temp);
+	    	retail.push(temp);
 	    });
 
-	    if(option == 1)
-	    	retail = data;
-	    else
-	    	wholesale = data;
+	    request(urlW, (error, response, body)=> {
+			let wholesale = [];
+		    let $ = cheerio.load(body);
+		    $('tr').each(function() {
+		    	let temp = [];
+		    	$(this).find('td').each(function(i) {
+		      		temp.push($(this).text());
+		    	});
+		    	wholesale.push(temp);
+		    });
+
+		    let reply = {
+		    	"retail"	: retail,
+		    	"wholesale"	: wholesale
+		    }
+		    res.send(reply);
+		});
 	});
-}
-
-app.get('/', (req, res) => {
-	getData(res, 1);
-	getData(res, 0);
-
-	let reply = {
-		"retail"	: retail,
-		"wholesale"	: wholesale
-	}
-
-	res.send(reply);
 })
 
 console.log('Server Up and running on PORT 3000!');
