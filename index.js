324f33bf9e0d280f7fe8aec2eb4b3e22b4098131
adityathebe <thebeaditya@gmail.com>
2017-07-02 07:28:51 +0545

    first

---
+++ index.js
@@ -0,0 +1,42 @@
+let request = require('request');
+let cheerio = require('cheerio');
+let express = require('express');
+let cors = require('cors');
+let app = express();
+app.use(cors());
+
+app.get('/whole', (req, res) => {
+	let url = "http://kalimatimarket.gov.np/home/wpricelist"
+	getData(url, res);
+})
+
+app.get('/retail', (req, res) => {
+	let url = "http://kalimatimarket.gov.np/home/rpricelist";
+	getData(url, res);
+})
+
+function getData(url, res) {
+	request(url, (error, response, body)=> {
+	    let data = [];
+
+	    /* ------ Cheerio -------- */
+	    let $ = cheerio.load(body);
+	    $('tr').each(function() {
+	    	let temp = [];
+	    	$(this).find('td').each(function(i) {
+	      		temp.push($(this).text());
+	    	});
+	    	data.push(temp);
+	    });
+
+	    let reply = {
+	    	length	: data.length,
+	    	items	: data
+	    }
+
+	    res.send(reply);
+	});
+}
+
+console.log('Server Up and running!');
+app.listen(process.env.PORT || 3000);
